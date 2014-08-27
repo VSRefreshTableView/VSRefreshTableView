da933dceb87a0cd3ee391f2d1110328d30d88a9d
allen.yang <allen.yang@vipshop.com>
2014-08-27 19:01:23 +0800

    add files

---
+++ EGORefreshTableFooterView.h
@@ -0,0 +1,13 @@
+//
+//  EGORefreshTableFooterView.h
+//  Viplux
+//
+//  Created by 杨进鹏 on 14-8-22.
+//  Copyright (c) 2014年 Vip. All rights reserved.
+//  扩展EGORefreshTableHeaderView 实现上拉加载更多功能
+
+#import "EGORefreshTableHeaderView.h"
+
+@interface EGORefreshTableFooterView : EGORefreshTableHeaderView
+
+@end
---
+++ EGORefreshTableFooterView.m
@@ -0,0 +1,22 @@
+//
+//  EGORefreshTableFooterView.m
+//  Viplux
+//
+//  Created by 杨进鹏 on 14-8-22.
+//  Copyright (c) 2014年 Vip. All rights reserved.
+//
+
+#import "EGORefreshTableFooterView.h"
+
+@implementation EGORefreshTableFooterView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
